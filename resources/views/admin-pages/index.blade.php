ce2b46d508ad220f71246ebdf45df4a5d7eac673
brunosusak <45066982+brunosusak@users.noreply.github.com>
2019-04-02 21:30:19 +0200

    Add files via upload

--- resources/views/admin-pages/index.blade.php
+++ resources/views/admin-pages/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-    hiiiiii
+    Ovo je admin panel, možete dodati, brisati i uređivati vlastite ponude tako što izaberete što želite na opciji "Ponuda".
 @endsection
